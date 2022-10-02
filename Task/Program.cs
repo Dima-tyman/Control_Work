226413e0d49dba91a2532352e9cc88908fb82ea9
Dima-tyman <tyman-dima@list.ru>
2022-10-02 21:24:41 +0300

    Add method -SelectValue-

--- Task/Program.cs
+++ Task/Program.cs
@@ -9,5 +9,15 @@ int GetLengthArray (string[] arr) {
     return sum;
 }
 
-Console.WriteLine(string.Join(" ", array));
-Console.WriteLine(GetLengthArray(array));
+void SelectValue (string[] newArr, string[] oldArr) {
+    for (int i = 0, j = 0; i < oldArr.Length; i++)
+        if (oldArr[i].Length <= 3) {
+        newArr[j] = oldArr[i];
+        j++;
+        }
+}
+
+Console.WriteLine(string.Join(", ", array));
+string[] newArray = new string[GetLengthArray(array)];
+SelectValue (newArray, array);
+Console.WriteLine(string.Join(", ", newArray));
