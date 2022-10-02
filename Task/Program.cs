35c3b5c427d1a5e6403f43abe3bbdeed226ce8c8
Dima-tyman <tyman-dima@list.ru>
2022-10-02 21:16:11 +0300

    Add method -GetLength

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.Clear();
 
+string[] array = {"hello", "2", "world", ":-)"};
+
+int GetLengthArray (string[] arr) {
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i].Length <= 3) sum++;
+    return sum;
+}
+
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine(GetLengthArray(array));
